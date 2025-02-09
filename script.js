7ccf960b90dcd9dea94f838f320ff1d87b746f0f
getishe <getahune.alemayhu@gmail.com>
2025-02-09 16:43:19 +0300

    Update the form validation

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+       const username = document.getElementById('username').value.trim()
+        const email =document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
+
+        console.log(username, email, password);
+
+        var isValid = true;
+        var message = [];
+
+        if (username.length < 3) {
+            isValid = false;
+            message.push('Username must be at least 3 characters long');
+        }
+        // if (email === '@' || email === '.'){
+         if(!email.include('@') || !email.include('.')){
+            isValid = false;
+            message
+        }
+        if(password.length < 8){
+            isValid = false;
+            message.push('Password must be at least 8 characters long');
+        }
+
+        feedbackDiv.style.display = 'block';
+
+        if(isValid){
+            feedbackDiv.style.color = "28a745";
+            feedbackDiv.textContent = ' Registration Successful';
+        }
+        else {
+            feedbackDiv.innerHTML = message.join('<br>');
+            feedbackDiv.style.color = "#dc3545";
+        }
+
+    });
+})
